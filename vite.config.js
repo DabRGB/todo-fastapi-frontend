f602883b7d76dbfeae4dc68873975db28cdbcf5d
DabRGB <clavite.dave09@gmail.com>
2025-04-13 23:31:29 +0800

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  server: {
-    fs: {
-      strict: false // Allows accessing files outside root
-    }
-  },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    sourcemap: true // Helps debug if needed
-  }
-});
+})
